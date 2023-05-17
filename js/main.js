cea25678b8710df705d0c91812c6094a2025dc39
Rian-usr <rian.jesus5@etec.sp.gov.br>
2023-05-16 22:38:02 -0300

    dropdonw

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
-function validarCPF(CPF) {
-  var Soma = 0
-  var Resto
-   
-  if(CPF == "00000000000") {
-    return alert("CPF inválido")
-  }
+const navMenu = document.getElementById('nav_menu'),
+      toggleMenu = document.getElementById('toggle-menu'),
+	  closeMenu = document.getElementById('close-menu')
 
-  for( i = 1; i < 9; i++) {
-    Soma = Soma + parseInt(CPF, substring(i - 1, i)) * (11 - 1)
-    Resto =(Soma * 10) % 11
-  }
-
-  if((Resto == 10) || (Resto == 11)) {
-    Resto = 0
-  }
-
-  if(Resto != parseInt(CPF.substring(10, 11))) {
-    return alert("CPF inválido")
-  }
-
-  return alert("CPF válido")
-}
+toggleMenu.addEventListener('click', () => {
+	navMenu.classList.toggle('show')
+})
+closeMenu.addEventListener('click', () => {
+	navMenu.classList.remove('show')
+})
